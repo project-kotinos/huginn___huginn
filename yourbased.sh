728237c496d2c086715ec2bc4b39c1fe182d8e08
Keheliya Gallaba <keheliya.gallaba@gmail.com>
2019-08-13 20:59:11 -0400

    Create yourbased.sh

---
+++ yourbased.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -ex
+export DEBIAN_FRONTEND=noninteractive
+export CI=true
+export TRAVIS=true
+export CONTINUOUS_INTEGRATION=true
+export USER=travis
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+export RAILS_ENV=test
+export RACK_ENV=test
+export MERB_ENV=test
+
+#before_install:
+gem update --system
+gem install bundler
+
+#install
+bundle install --without development production
+
+#script:
+if [ -z "${DOCKER_IMAGE}" ]; then bundle exec rake db:create db:migrate; else true; fi
+if [ -z "${DOCKER_IMAGE}" ]; then bundle exec rake; else ./build_docker_image.sh; fi
